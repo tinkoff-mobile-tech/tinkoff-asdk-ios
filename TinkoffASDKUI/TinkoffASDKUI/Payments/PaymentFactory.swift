c3fffa9f833517cc0b6734ca9f5bebea7fba900c
Grigory Serebryaniy <g.serebryaniy@tinkoff.ru>
2021-03-05 16:01:05 +0300

    payment logic
    - add PaymentFactory

---
+++ TinkoffASDKUI/TinkoffASDKUI/Payments/PaymentFactory.swift
@@ -0,0 +1,44 @@
+//
+//
+//  PaymentFactory.swift
+//
+//  Copyright (c) 2021 Tinkoff Bank
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+
+import TinkoffASDKCore
+
+struct PaymentFactory {
+    private let acquiringSDK: AcquiringSdk
+    
+    init(acquiringSDK: AcquiringSdk) {
+        self.acquiringSDK = acquiringSDK
+    }
+    
+    func createPayment(paymentSource: PaymentSourceData,
+                       paymentFlow: PaymentFlow) -> Payment {
+        switch paymentSource {
+        case .cardNumber, .savedCard, .paymentData:
+            return CardPayment(acquiringSDK: acquiringSDK,
+                               paymentSource: paymentSource,
+                               paymentFlow: paymentFlow)
+        case .parentPayment:
+            print("return Charge")
+            fatalError()
+        case .unknown:
+            fatalError()
+        }
+    }
+}
