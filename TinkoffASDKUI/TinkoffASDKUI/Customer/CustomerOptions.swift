61c8f736705be9b44d7c2aa2bd466f748cead097
Grigory Serebryaniy <g.serebryaniy@tinkoff.ru>
2021-03-05 15:34:44 +0300

    payment logic
    - add CustomerOptions
    - add OrderOptions
    - add PaymentOptions
    - add PaymentInitData+PaymentOptions extension to build PaymentInitData from PaymentOptions

---
+++ TinkoffASDKUI/TinkoffASDKUI/Customer/CustomerOptions.swift
@@ -0,0 +1,37 @@
+//
+//
+//  CustomerOptions.swift
+//
+//  Copyright (c) 2021 Tinkoff Bank
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+
+import TinkoffASDKCore
+
+public struct CustomerOptions {
+    public enum Customer {
+        case none
+        case customer(key: String, checkType: PaymentCardCheckType)
+    }
+    
+    let customer: Customer
+    let email: String?
+    
+    public init(customer: Customer = .none,
+                email: String? = nil) {
+        self.customer = customer
+        self.email = email
+    }
+}
