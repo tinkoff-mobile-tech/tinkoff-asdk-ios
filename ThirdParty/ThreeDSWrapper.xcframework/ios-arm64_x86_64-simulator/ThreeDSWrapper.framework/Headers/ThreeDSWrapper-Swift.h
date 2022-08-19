0ec30ade11a1e88eec0480fa7957a666c3519c09
i.bazarov <i.bazarov@tinkoff.ru>
2022-08-19 19:08:30 +0300

    update threedswrapper & tdssdkios

--- ThirdParty/ThreeDSWrapper.xcframework/ios-arm64_x86_64-simulator/ThreeDSWrapper.framework/Headers/ThreeDSWrapper-Swift.h
+++ ThirdParty/ThreeDSWrapper.xcframework/ios-arm64_x86_64-simulator/ThreeDSWrapper.framework/Headers/ThreeDSWrapper-Swift.h
@@ -208,7 +208,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 
-
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
 #endif
@@ -422,7 +421,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # pragma clang attribute push(__attribute__((external_source_symbol(language="Swift", defined_in="ThreeDSWrapper",generated_declaration))), apply_to=any(function,enum,objc_interface,objc_category,objc_protocol))
 # pragma pop_macro("any")
 #endif
-
 
 
 #if __has_attribute(external_source_symbol)
