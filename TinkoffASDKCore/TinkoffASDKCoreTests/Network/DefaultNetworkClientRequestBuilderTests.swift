5d290cdcc324bc6c4446b3dedd7e585b69ac4ecd
Nikita Vasilev <nv3212@gmail.com>
2022-08-30 09:20:55 +0300

    network layer tests
    - add TestsNetworkRequest in ASDKCore tests to be able to create any request
    - add DefaultNetworkClientRequestBuilder tests to check if url builded correct

---
+++ TinkoffASDKCore/TinkoffASDKCoreTests/Network/DefaultNetworkClientRequestBuilderTests.swift
@@ -0,0 +1,62 @@
+//
+//
+//  DefaultNetworkClientRequestBuilderTests.swift
+//
+//  Copyright (c) 2021 Tinkoff Bank
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+
+@testable import TinkoffASDKCore
+import XCTest
+
+class DefaultNetworkClientRequestBuilderTests: XCTestCase {
+    
+    let builder = DefaultNetworkClientRequestBuilder()
+    let baseURL = URL(string: "https://tinkoff.ru")!
+    
+    func testNoErrorBuildWithCorrectURLAndPath() {
+        let request = TestsNetworkRequest(path: ["test"], httpMethod: .get)
+        XCTAssertNoThrow(try builder.buildURLRequest(baseURL: baseURL, request: request, requestAdapter: nil))
+    }
+    
+    func testBuildFailedWithErrorWithEmptyPath() {
+        let request = TestsNetworkRequest(path: [], httpMethod: .get)
+        XCTAssertThrowsError(try builder.buildURLRequest(baseURL: baseURL, request: request, requestAdapter: nil))
+    }
+
+    func testBuildCorrectUrlWithOneItemPath() {
+        let request = TestsNetworkRequest(path: ["test"], httpMethod: .get)
+        let resultURLString = "https://tinkoff.ru/test"
+        do {
+            let urlRequest = try builder.buildURLRequest(baseURL: baseURL, request: request, requestAdapter: nil)
+            XCTAssertNotNil(urlRequest.url?.absoluteString, "URLRequest's url can't be nil")
+            XCTAssertEqual(urlRequest.url?.absoluteString, resultURLString)
+        } catch {
+            XCTFail("URLRequest build failed with: \(error)")
+        }
+    }
+    
+    func testBuildCorrectUrlWithThreeItemsPath() {
+        let request = TestsNetworkRequest(path: ["test", "url", "builder"], httpMethod: .get)
+        let resultURLString = "https://tinkoff.ru/test/url/builder"
+        do {
+            let urlRequest = try builder.buildURLRequest(baseURL: baseURL, request: request, requestAdapter: nil)
+            XCTAssertNotNil(urlRequest.url?.absoluteString, "URLRequest's url can't be nil")
+            XCTAssertEqual(urlRequest.url?.absoluteString, resultURLString)
+        } catch {
+            XCTFail("URLRequest build failed with: \(error)")
+        }
+    }
+}
