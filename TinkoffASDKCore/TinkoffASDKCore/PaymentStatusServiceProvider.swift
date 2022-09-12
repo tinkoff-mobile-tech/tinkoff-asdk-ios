9a29a9045bdd101048b49b170f6aefdc8b46c2b2
Ruslan Akhmadeev <r.akhmadeev@tinkoff.ru>
2022-09-12 16:09:57 +0300

    Revert changes in PaymentStatusServiceProvider

--- TinkoffASDKCore/TinkoffASDKCore/PaymentStatusServiceProvider.swift
+++ TinkoffASDKCore/TinkoffASDKCore/PaymentStatusServiceProvider.swift
@@ -20,15 +20,15 @@
 import Foundation
 
 public final class PaymentStatusServiceProvider: FetchServiceProtocol {
-    public typealias ObjectType = GetPaymentStatePayload
+    public typealias ObjectType = PaymentStatusResponse
 
     /// Текущее состояние сервиса проверки
-    public var fetchStatus: FetchStatus<GetPaymentStatePayload> = .unknown
+    public var fetchStatus: FetchStatus<PaymentStatusResponse> = .unknown
     var queryStatus: Cancellable?
     /// Слушатель состояния сервиса проверки
-    public var onStatusUpdated: ((FetchStatus<GetPaymentStatePayload>) -> Void)?
+    public var onStatusUpdated: ((FetchStatus<PaymentStatusResponse>) -> Void)?
     /// Платеж состояние которого проверяем
-    private(set) var paymentId: PaymentId = ""
+    private(set) var paymentId: Int64 = 0
     /// Частота обновления, не менее 10 сек
     public var updateTimeInterval: TimeInterval = 5 {
         didSet {
@@ -40,7 +40,7 @@ public final class PaymentStatusServiceProvider: FetchServiceProtocol {
 
     private weak var sdk: AcquiringSdk?
 
-    public init(sdk: AcquiringSdk?, paymentId: PaymentId, updateTimeInterval: TimeInterval = 5) {
+    public init(sdk: AcquiringSdk?, paymentId: Int64, updateTimeInterval: TimeInterval = 5) {
         self.sdk = sdk
         self.paymentId = paymentId
         self.updateTimeInterval = updateTimeInterval
@@ -65,12 +65,12 @@ public final class PaymentStatusServiceProvider: FetchServiceProtocol {
 
     // MARK: FetchDataSourceProtocol
 
-    public func fetch(startHandler: (() -> Void)?, completeHandler: @escaping (GetPaymentStatePayload?, Error?) -> Void) {
+    public func fetch(startHandler: (() -> Void)?, completeHandler: @escaping (PaymentStatusResponse?, Error?) -> Void) {
         fetchStatus = .loading
         startHandler?()
 
-        queryStatus = sdk?.paymentOperationStatus(data: PaymentInfoData(paymentId: paymentId), completionHandler: { [weak self] (response: Result<GetPaymentStatePayload, Error>) in
-            var status: FetchStatus<GetPaymentStatePayload> = .loading
+        queryStatus = sdk?.paymentOperationStatus(data: PaymentInfoData(paymentId: paymentId), completionHandler: { [weak self] response in
+            var status: FetchStatus<PaymentStatusResponse> = .loading
             switch response {
             case let .failure(error):
                 status = FetchStatus.error(error)
