b41acc619e49b368d54a47d06f32ffde6219b941
Ivan Glushko <ivanglushkodev@yandex.ru>
2022-09-12 09:47:49 +0200

    fixed uppercased localized strings

--- TinkoffASDKUI/TinkoffASDKUI/Resources/Generated/Strings+Generated.swift
+++ TinkoffASDKUI/TinkoffASDKUI/Resources/Generated/Strings+Generated.swift
@@ -255,19 +255,7 @@ internal enum Loc {
 
 extension Loc {
   private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
-    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    let format = Bundle.uiResources.localizedString(forKey: key, value: value, table: table)
     return String(format: format, locale: Locale.current, arguments: args)
   }
 }
-
-// swiftlint:disable convenience_type
-private final class BundleToken {
-  static let bundle: Bundle = {
-    #if SWIFT_PACKAGE
-    return Bundle.module
-    #else
-    return Bundle(for: BundleToken.self)
-    #endif
-  }()
-}
-// swiftlint:enable convenience_type
