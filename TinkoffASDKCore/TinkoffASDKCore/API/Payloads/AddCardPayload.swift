97d23e888f3d4d0da64eab3a04397e19bd6c9a96
Ruslan Akhmadeev <r.akhmadeev@tinkoff.ru>
2022-09-13 12:39:56 +0300

    AddCardPayload style

--- TinkoffASDKCore/TinkoffASDKCore/API/Payloads/AddCardPayload.swift
+++ TinkoffASDKCore/TinkoffASDKCore/API/Payloads/AddCardPayload.swift
@@ -36,9 +36,4 @@ public struct AddCardPayload: Decodable, Equatable {
     public init(requestKey: String) {
         self.requestKey = requestKey
     }
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        requestKey = try container.decode(String.self, forKey: .requestKey)
-    }
 }
