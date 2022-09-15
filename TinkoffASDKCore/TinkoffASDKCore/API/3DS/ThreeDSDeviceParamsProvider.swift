34b1795fc29c8dac8e19871752b62a05d689035f
Ruslan Akhmadeev <r.akhmadeev@tinkoff.ru>
2022-09-15 13:48:27 +0300

    Add CoreGraphics import for SPM

--- TinkoffASDKCore/TinkoffASDKCore/API/3DS/ThreeDSDeviceParamsProvider.swift
+++ TinkoffASDKCore/TinkoffASDKCore/API/3DS/ThreeDSDeviceParamsProvider.swift
@@ -17,6 +17,7 @@
 //  limitations under the License.
 //
 
+import struct CoreGraphics.CGSize
 import Foundation
 
 public protocol ThreeDSDeviceParamsProvider {
--- TinkoffASDKCore/TinkoffASDKCore/AcquiringSdk.swift
+++ TinkoffASDKCore/TinkoffASDKCore/AcquiringSdk.swift
@@ -17,6 +17,7 @@
 //  limitations under the License.
 //
 
+import struct CoreGraphics.CGSize
 import Foundation
 import UIKit
 
