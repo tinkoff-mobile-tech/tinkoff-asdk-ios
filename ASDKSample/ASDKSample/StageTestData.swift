a5765904d2321d63dc4df113ae4e53140832f451
Ruslan Akhmadeev <r.akhmadeev@tinkoff.ru>
2022-10-10 11:15:44 +0300

    Fix staged data linking

--- ASDKSample/ASDKSample/StageTestData.swift
+++ ASDKSample/ASDKSample/StageTestData.swift
@@ -22,6 +22,7 @@ import Foundation
 /// Тестовые данные для проведения тестовых платежей
 public enum StageTestData {
     /// Открытый ключ для шифрования карточных данных (номер карты, срок дейсвия и секретный код)
+    // swiftlint:disable:next line_length
     public static let testPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqBiorLS9OrFPezixO5lSsF+HiZPFQWDO7x8gBJp4m86Wwz7ePNE8ZV4sUAZBqphdqSpXkybM4CJwxdj5R5q9+RHsb1dbMjThTXniwPpJdw4WKqG5/cLDrPGJY9NnPifBhA/MthASzoB+60+jCwkFmf8xEE9rZdoJUc2p9FL4wxKQPOuxCqL2iWOxAO8pxJBAxFojioVu422RWaQvoOMuZzhqUEpxA9T62lN8t3jj9QfHXaL4Ht8kRaa2JlaURtPJB5iBM+4pBDnqObNS5NFcXOxloZX4+M8zXaFh70jqWfiCzjyhaFg3rTPE2ClseOdS7DLwfB2kNP3K0GuPuLzsMwIDAQAB"
 
     /// Уникальный идентификатор терминала, выдается Продавцу Банком на каждый магазин.
