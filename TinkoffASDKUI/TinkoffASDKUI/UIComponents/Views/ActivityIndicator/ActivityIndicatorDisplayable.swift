d8415751e6f487486802d70d40a0d2fe75afbac1
Ivan Glushko <ivanglushkodev@yandex.ru>
2022-11-16 11:56:33 +0400

    [MIC-6828] Added Stylable Button (#219)
    * [MIC-6828] Added Stylable Button

---
+++ TinkoffASDKUI/TinkoffASDKUI/UIComponents/Views/ActivityIndicator/ActivityIndicatorDisplayable.swift
@@ -0,0 +1,53 @@
+//
+//  ActivityIndicatorDisplayable.swift
+//  popup
+//
+//  Created by Ivan Glushko on 14.11.2022.
+//
+
+import UIKit
+
+/// Протокол с возможностью показа / скрытия индикатора
+protocol ActivityIndicatorDisplayable {
+    /// Показать индикатор
+    func showActivityIndicator(with style: ActivityIndicatorView.Style)
+    /// Скрыть индикатор
+    func hideActivityIndicator()
+}
+
+/// Расширение протокола для возможности показывать индикатор
+extension ActivityIndicatorDisplayable where Self: UIViewController {
+    /// Показать индикатор
+    func showActivityIndicator(with style: ActivityIndicatorView.Style) {
+        let activityIndicatorView = ActivityIndicatorView(style: style)
+        activityIndicatorView.transform = CGAffineTransform(scaleX: .zero, y: .zero)
+
+        let container = ViewContainer(base: activityIndicatorView)
+
+        view.addSubview(container)
+
+        container.makeEqualToSuperview()
+        UIView.animate(withDuration: .scaleDuration) {
+            activityIndicatorView.transform = .identity
+        }
+
+        activityIndicatorView.startAnimation(animated: true)
+    }
+
+    /// Скрыть индикатор
+    func hideActivityIndicator() {
+        let container = view.subviews.compactMap { $0 as? ViewContainer<ActivityIndicatorView> }.first
+        let indicatorView = container?.base
+        UIView.animate(withDuration: .scaleDuration, animations: {
+            indicatorView?.alpha = .zero
+        }, completion: { _ in
+            container?.removeFromSuperview()
+        })
+    }
+}
+
+// MARK: - Constants
+
+extension TimeInterval {
+    static let scaleDuration = 0.25 as TimeInterval
+}
---
+++ TinkoffASDKUI/TinkoffASDKUI/UIComponents/Views/ActivityIndicator/ViewContainer.swift
@@ -0,0 +1,36 @@
+//
+//  ViewContainer.swift
+//  popup
+//
+//  Created by Ivan Glushko on 14.11.2022.
+//
+
+import UIKit
+
+final class ViewContainer<T: UIView>: UIView {
+    var base: T
+
+    init(base: T) {
+        self.base = base
+        super.init(frame: .zero)
+
+        setup()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("Has not been implemented!!!")
+    }
+
+    private func setup() {
+        backgroundColor = .clear
+        addSubview(base)
+
+        base.makeConstraints { make in
+            [
+                make.centerXAnchor.constraint(equalTo: make.forcedSuperview.centerXAnchor),
+                make.centerYAnchor.constraint(equalTo: make.forcedSuperview.centerYAnchor),
+            ]
+        }
+    }
+}
