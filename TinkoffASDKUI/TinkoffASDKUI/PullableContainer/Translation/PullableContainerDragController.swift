0e40391580f7bfb7c7cbc4a173ca241d9f15d2d5
Ruslan Akhmadeev <akhaman@yandex.ru>
2022-12-16 14:35:01 +0300

    [MIC-7349] Add ability to provide dismissing behavior to PullableContainer (#237)
    * Add ability to provide dismissing behavior to PullableContainer
    * Update CHANGELOG

--- TinkoffASDKUI/TinkoffASDKUI/PullableContainer/Translation/PullableContainerDragController.swift
+++ TinkoffASDKUI/TinkoffASDKUI/PullableContainer/Translation/PullableContainerDragController.swift
@@ -20,24 +20,25 @@
 import UIKit
 
 protocol PullableContainerDragControllerDelegate: AnyObject {
-    func pullableContainerDragControllerMaximumContentHeight(_ controller: PullableContainerDragController) -> CGFloat
+    func pullableContainerDragControllerDidRequestMaxContentHeight(_ controller: PullableContainerDragController) -> CGFloat
     func pullableContainerDragControllerDidEndDragging(_ controller: PullableContainerDragController)
     func pullableContainerDragControllerDidCloseContainer(_ controller: PullableContainerDragController)
+    func pullableContainerDragControllerShouldDismissOnDownDragging(_ controller: PullableContainerDragController) -> Bool
 }
 
 final class PullableContainerDragController {
-
     weak var delegate: PullableContainerDragControllerDelegate?
-
+    var insets: UIEdgeInsets = .zero
     private let dragViewHeightConstraint: NSLayoutConstraint
-
     private var dragViewHeight: CGFloat = 0
 
-    var insets: UIEdgeInsets = .zero
+    // MARK: Init
 
     init(dragViewHeightConstraint: NSLayoutConstraint) {
         self.dragViewHeightConstraint = dragViewHeightConstraint
     }
+
+    // MARK: Internal
 
     func setDefaultPositionWithContentHeight(_ contentHeight: CGFloat) {
         dragViewHeight = contentHeight + insets.bottom + insets.top
@@ -53,8 +54,10 @@ final class PullableContainerDragController {
         velocity: CGFloat
     ) {
         let dragProportion = offset / dragViewHeight
-        let isDismiss = dragProportion >= .dismissDragProportionTreshold || velocity > .dismissVelocityTreshold
-        if isDismiss {
+        let isDismissingDrag = dragProportion >= .dismissDragProportionTreshold || velocity > .dismissVelocityTreshold
+        let isDismissionAllowed = delegate?.pullableContainerDragControllerShouldDismissOnDownDragging(self) ?? true
+
+        if isDismissingDrag, isDismissionAllowed {
             delegate?.pullableContainerDragControllerDidCloseContainer(self)
         } else {
             dragViewHeightConstraint.constant = dragViewHeight
@@ -62,21 +65,57 @@ final class PullableContainerDragController {
         }
     }
 
+    // MARK: Helpers
+
     private func calculateDragViewHeight(offset: CGFloat) -> CGFloat {
-        let resultOffset = max(-.maximumDragOffset, offset < 0 ? offset / 2 : offset)
+        offset < 0
+            ? calculateDragViewHeightForTopDirectionDragging(offset: offset)
+            : calculateDragViewHeightForBottomDirectionDragging(offset: offset)
+    }
+
+    private func calculateDragViewHeightForTopDirectionDragging(offset: CGFloat) -> CGFloat {
+        assert(offset <= 0)
+        let resultOffset = max(-.maximumDragOffset, offset / 2)
         let resultHeight = min(maximumDragViewHeight(), dragViewHeight - resultOffset)
         return resultHeight
+    }
+
+    private func calculateDragViewHeightForBottomDirectionDragging(offset: CGFloat) -> CGFloat {
+        assert(offset >= 0)
+
+        if shouldDismissOnDownDragging() {
+            return dragViewHeight - offset
+        } else {
+            let resultOffset = min(.maximumDragOffset, offset / 2)
+            return max(dragViewHeight - resultOffset, dragViewHeight - .maximumDragOffset)
+        }
     }
 
     private func maximumDragViewHeight() -> CGFloat {
         guard let delegate = delegate else {
             return dragViewHeight
         }
-        let maximumHeight = delegate.pullableContainerDragControllerMaximumContentHeight(self)
+
+        let maximumHeight = delegate.pullableContainerDragControllerDidRequestMaxContentHeight(self)
             + insets.bottom
             + insets.top
+
         return maximumHeight
     }
+
+    private func shouldDismissOnDownDragging() -> Bool {
+        guard let delegate = delegate else {
+            return .defaultShouldDismissOnDownDragging
+        }
+
+        return delegate.pullableContainerDragControllerShouldDismissOnDownDragging(self)
+    }
+}
+
+// MARK: - Constants
+
+private extension Bool {
+    static let defaultShouldDismissOnDownDragging = true
 }
 
 private extension CGFloat {
