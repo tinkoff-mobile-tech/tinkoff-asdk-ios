cb7d00171a4f84fcf16bbbd52fe6b71637ee33c7
Aleksandr Pravosudov <a.pravosudov@tinkoff.ru>
2023-01-12 14:14:16 +0400

    MIC-7380: Переделал коммент для asyncDeduped

--- TinkoffASDKUI/TinkoffASDKUI/Common/Extensions/DispatchQueue+Ext.swift
+++ TinkoffASDKUI/TinkoffASDKUI/Common/Extensions/DispatchQueue+Ext.swift
@@ -6,22 +6,20 @@
 //
 
 extension DispatchQueue {
-
-    /**
-     - parameters:
-        - target: Объект, используемый в качестве отслеживаемого, в большинстве случаев self.
-        - delay: Время задержки
-        - work: работа которая должна быть выполнена
-     Выполняет работу только один раз для данного target(а), учитывая временное окно. Последняя добавленная кложура, это та,
-     которая будет выполнена в конце.
-     Примечание. Безопасно вызывать только из  main треда.
-     Пример использования:
-     ```
-     DispatchQueue.main.asyncDeduped(target: self, after: 1.0) { [weak self] in
-         self?.doTheWork()
-     }
-     ```
+    
+    ///  Выполняет работу только один раз для данного target(а), учитывая временное окно. Последняя добавленная кложура, это та,
+    ///  которая будет выполнена в конце.
+    ///  Примечание. Безопасно вызывать только из  main треда.
+    ///  Пример использования:
+    /*
+    DispatchQueue.main.asyncDeduped(target: self, after: 1.0) { [weak self] in
+        self?.doTheWork()
+    }
      */
+    /// - Parameters:
+    ///   - target: Объект, используемый в качестве отслеживаемого, в большинстве случаев self.
+    ///   - delay: Время задержки
+    ///   - work: работа которая должна быть выполнена
     func asyncDeduped(target: AnyObject, after delay: TimeInterval, execute work: @escaping @convention(block) () -> Void) {
         let dedupeIdentifier = DispatchQueue.dedupeIdentifierFor(target)
         if let existingWorkItem = DispatchQueue.workItems.removeValue(forKey: dedupeIdentifier) {
