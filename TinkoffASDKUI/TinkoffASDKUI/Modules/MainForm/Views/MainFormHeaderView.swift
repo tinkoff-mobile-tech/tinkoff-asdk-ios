7fffa66d5a17ea55d3159fbe3211aebc35e45370
Ruslan Akhmadeev <r.akhmadeev@tinkoff.ru>
2023-01-20 10:41:39 +0300

    MIC-7704: Add subviews for MainFormHeaderView

--- TinkoffASDKUI/TinkoffASDKUI/Modules/MainForm/Views/MainFormHeaderView.swift
+++ TinkoffASDKUI/TinkoffASDKUI/Modules/MainForm/Views/MainFormHeaderView.swift
@@ -12,6 +12,46 @@ final class MainFormHeaderView: UIView {
 
     private lazy var activityIndicator = ActivityIndicatorView(style: .xlYellow)
 
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+
+    // MARK: Text Subviews
+
+    private lazy var textStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.spacing = 8
+        return stack
+    }()
+
+    private lazy var amountDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .regular)
+        label.textColor = ASDKColors.Text.secondary.color
+        label.textAlignment = .center
+        return label
+    }()
+
+    private lazy var amountLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 34, weight: .bold)
+        label.textAlignment = .center
+        return label
+    }()
+
+    private lazy var orderDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .regular)
+        label.textColor = ASDKColors.Text.secondary.color
+        label.textAlignment = .center
+        return label
+    }()
+
+    private lazy var payButton = Button()
+
     // MARK: Init
 
     override init(frame: CGRect) {
@@ -55,4 +95,5 @@ extension MainFormHeaderView {
 
 private extension CGFloat {
     static let indicatorVerticalInsets: CGFloat = 32
+    static let commonHorizontalInsets: CGFloat = 16
 }
