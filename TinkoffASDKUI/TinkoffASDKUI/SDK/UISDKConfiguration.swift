68b204ff0ed02380b3c79ac8f1db815ff645bc2f
Aleksandr Pravosudov <a.pravosudov@tinkoff.ru>
2023-02-08 23:57:56 +0400

    MIC-7945: Удалил SBPConfiguration из UISDKConfiguration

--- TinkoffASDKUI/TinkoffASDKUI/SDK/UISDKConfiguration.swift
+++ TinkoffASDKUI/TinkoffASDKUI/SDK/UISDKConfiguration.swift
@@ -12,37 +12,9 @@ public struct UISDKConfiguration {
     /// Запрашивает данные и способ аутентификация для `WKWebView`
     let webViewAuthChallengeService: IWebViewAuthChallengeService?
 
-    /// Доступная конфигурация раздела с оплатой через систему быстрых платежей (СБП)
-    let sbpConfiguration: SBPConfiguration
-
     /// Инициалищация конфигурации `TinkoffASDKUI`
     /// - Parameter webViewAuthChallengeService: Запрашивает данные и способ аутентификация для `WKWebView`
-    /// - Parameter sbpConfiguration: конфигурация раздела системы быстрых платежей (СБП)
-    public init(
-        webViewAuthChallengeService: IWebViewAuthChallengeService? = nil,
-        sbpConfiguration: SBPConfiguration = SBPConfiguration()
-    ) {
+    public init(webViewAuthChallengeService: IWebViewAuthChallengeService? = nil) {
         self.webViewAuthChallengeService = webViewAuthChallengeService
-        self.sbpConfiguration = sbpConfiguration
-    }
-}
-
-public struct SBPConfiguration {
-
-    // MARK: Properties
-
-    /// Отвечает за максимальное количество запросов на обновление статуса, можно установить любое значение
-    ///
-    /// При выборе банка, при оплате через СБП, осуществляется переход в приложение банка.
-    /// В  этот момент будет отображаться шторка с информацией о статусе оплаты юзером.
-    /// Запросы обновления статуса осуществляются с минимальным интервалом в 3 секунды между друг другом
-    /// По умолчанию будет осуществлено 10 запросов, по истечении которых юзер получит уведомление об истечении времени,
-    /// отведенного на оплату
-    let paymentStatusRetriesCount: Int
-
-    // MARK: Initialization
-
-    public init(paymentStatusRetriesCount: Int = 10) {
-        self.paymentStatusRetriesCount = paymentStatusRetriesCount
     }
 }
