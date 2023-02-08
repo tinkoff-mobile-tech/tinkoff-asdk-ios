dea235387e654890c8265caf21347b28685dae88
Aleksandr Pravosudov <a.pravosudov@tinkoff.ru>
2023-02-08 21:35:42 +0400

    MIC-7945: Добавил PaymentStatusService

---
+++ TinkoffASDKUI/TinkoffASDKUI/SBP/Modules/SBPPaymentSheet/Services/PaymentStatusService/IPaymentStatusService.swift
@@ -0,0 +1,14 @@
+//
+//  IPaymentStatusService.swift
+//  TinkoffASDKUI
+//
+//  Created by Aleksandr Pravosudov on 17.01.2023.
+//
+
+import TinkoffASDKCore
+
+typealias PaymentStatusServiceCompletion = (Result<GetPaymentStatePayload, Error>) -> Void
+
+protocol IPaymentStatusService {
+    func getPaymentStatus(paymentId: String, completion: @escaping PaymentStatusServiceCompletion)
+}
