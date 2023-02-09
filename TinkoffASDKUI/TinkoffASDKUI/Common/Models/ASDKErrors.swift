02c25b9777ce277c30d00769ce05cbdcf476a558
Ruslan Akhmadeev <r.akhmadeev@tinkoff.ru>
2023-02-09 19:22:26 +0300

    MIC-7945: Add new ASDKError model

--- TinkoffASDKUI/TinkoffASDKUI/Common/Models/ASDKErrors.swift
+++ TinkoffASDKUI/TinkoffASDKUI/Common/Models/ASDKErrors.swift
@@ -26,3 +26,58 @@ public enum ASDKErrors {
         }
     }
 }
+
+public struct ASDKError: Error {
+    public enum Code: Int {
+        case timeout = 123_001
+        case rejected = 123_002
+    }
+
+    public let code: Code
+    public let underlyingError: Error?
+
+    public init(code: Code, underlyingError: Error? = nil) {
+        self.code = code
+        self.underlyingError = underlyingError
+    }
+}
+
+// MARK: - LocalizedError
+
+extension ASDKError: LocalizedError {
+    public var errorDescription: String? {
+        let codeDescription: String = {
+            switch code {
+            case .timeout:
+                return "Time or retries count is over"
+            case .rejected:
+                return "Payment failed"
+            }
+        }()
+
+        let underlyingErrorDescription = underlyingError.map {
+            "Underlying error: \($0.localizedDescription)"
+        }
+
+        let fullDescription = [codeDescription, underlyingErrorDescription]
+            .compactMap { $0 }
+            .joined(separator: "; ")
+
+        return fullDescription
+    }
+}
+
+// MARK: - CustomNSError
+
+extension ASDKError: CustomNSError {
+    public var errorCode: Int { code.rawValue }
+
+    public var errorUserInfo: [String: Any] {
+        let userInfo: [String: Any?] = [
+            NSLocalizedDescriptionKey: errorDescription,
+            NSUnderlyingErrorKey: underlyingError,
+        ]
+
+        return userInfo.compactMapValues { $0 }
+    }
+}
