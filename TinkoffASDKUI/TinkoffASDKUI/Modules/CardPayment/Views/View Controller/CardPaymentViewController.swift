5f26fb15d9b0921534a7849908b18c56d0b34fd8
Aleksandr Pravosudov <a.pravosudov@tinkoff.ru>
2023-02-13 13:05:56 +0400

    MIC-7980: Добавил в иерархию веб вью

--- TinkoffASDKUI/TinkoffASDKUI/Modules/CardPayment/Views/View Controller/CardPaymentViewController.swift
+++ TinkoffASDKUI/TinkoffASDKUI/Modules/CardPayment/Views/View Controller/CardPaymentViewController.swift
@@ -46,6 +46,7 @@ final class CardPaymentViewController: UIViewController, ICardPaymentViewControl
         super.viewDidLoad()
         setupView()
         setupNavigationBar()
+        setupWebView()
         setupTableView()
 
         presenter.viewDidLoad()
@@ -170,6 +171,22 @@ extension CardPaymentViewController {
 
         navigationItem.leftBarButtonItem = leftItem
     }
+    
+    /// Не удалять, необходимо для корректной работы WebView
+    private func setupWebView() {
+        view.addSubview(webView)
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.topAnchor, constant: -.webViewTopInset),
+            webView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            webView.heightAnchor.constraint(equalToConstant: .webViewSideSize),
+            webView.widthAnchor.constraint(equalToConstant: .webViewSideSize)
+        ])
+        
+        webView.isHidden = true
+    }
 
     private func setupTableView() {
         view.addSubview(tableView)
@@ -194,6 +211,9 @@ extension CardPaymentViewController {
 
 private extension CGFloat {
     static let commonCellHeight: CGFloat = 56
+    
+    static let webViewTopInset: CGFloat = 200
+    static let webViewSideSize: CGFloat = 1
 }
 
 private extension UIEdgeInsets {
