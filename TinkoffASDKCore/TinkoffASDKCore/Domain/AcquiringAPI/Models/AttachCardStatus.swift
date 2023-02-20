9049e74972f7efdef766f9db3df240fd91a01ec8
Ruslan Akhmadeev <r.akhmadeev@tinkoff.ru>
2023-02-20 13:33:05 +0300

    MIC-4650: Add todo mark

--- TinkoffASDKCore/TinkoffASDKCore/Domain/AcquiringAPI/Models/AttachCardStatus.swift
+++ TinkoffASDKCore/TinkoffASDKCore/Domain/AcquiringAPI/Models/AttachCardStatus.swift
@@ -24,6 +24,7 @@ public enum AttachCardStatus {
     case needConfirmation3DS(Confirmation3DSData)
     /// Требуется подтверждение 3DS v2.0
     case needConfirmation3DSACS(Confirmation3DSDataACS)
+    // TODO: MIC-8072 Удалить неподдерживаемый статус
     /// Требуется подтвердить оплату указать сумму из смс для `requestKey`
     case needConfirmationRandomAmount(String)
     /// Успешная оплата
