2e4232d61ec23b4cfc282b614c7a915814f55130
Ruslan Akhmadeev <r.akhmadeev@tinkoff.ru>
2023-03-14 10:59:01 +0300

    MIC-8096: Refactor GetTinkoffPayStatus Request

--- TinkoffASDKCore/TinkoffASDKCore/Domain/AcquiringAPI/Responses/Payloads/GetTinkoffPayStatusPayload.swift
+++ TinkoffASDKCore/TinkoffASDKCore/Domain/AcquiringAPI/Responses/Payloads/GetTinkoffPayStatusPayload.swift
@@ -20,75 +20,44 @@
 import Foundation
 
 public struct GetTinkoffPayStatusPayload {
-    public enum Status: Codable {
-        public enum Version: String, Codable {
-            case version1 = "1.0"
-            case version2 = "2.0"
-        }
-
-        private enum CodingKeys: String, CodingKey {
-            case isAllowed = "Allowed"
-            case version = "Version"
-        }
-
+    public enum Status {
         case disallowed
-        case allowed(version: Version)
+        case allowed(TinkoffPayMethod)
+    }
 
-        public init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            let isAllowed = try container.decode(Bool.self, forKey: .isAllowed)
-            if isAllowed {
-                let version = try container.decode(Version.self, forKey: .version)
-                self = .allowed(version: version)
-            } else {
-                self = .disallowed
-            }
-        }
+    public let status: Status
+}
 
-        public func encode(to encoder: Encoder) throws {
-            var container = encoder.container(keyedBy: CodingKeys.self)
+// MARK: - GetTinkoffPayStatusPayload + Decodable
+
+extension GetTinkoffPayStatusPayload: Decodable {
+    private enum CodingKeys: CodingKey {
+        case status
+
+        var stringValue: String {
             switch self {
-            case let .allowed(version):
-                try container.encode(true, forKey: .isAllowed)
-                try container.encode(version, forKey: .version)
-            case .disallowed:
-                try container.encode(false, forKey: .isAllowed)
+            case .status: return Constants.Keys.params
             }
         }
     }
-
-    public let success: Bool
-    public let errorCode: Int
-    public let errorMessage: String?
-    public let errorDetails: String?
-    public let status: Status
-    public let terminalKey: String? = nil
 }
 
-extension GetTinkoffPayStatusPayload: Decodable {
+// MARK: - GetTinkoffPayStatusPayload.Status + Decodable
+
+extension GetTinkoffPayStatusPayload.Status: Decodable {
     private enum CodingKeys: String, CodingKey {
-        case success = "Success"
-        case errorCode = "ErrorCode"
-        case errorMessage = "Message"
-        case errorDetails = "Details"
-        case params = "Params"
+        case isAllowed = "Allowed"
+        case version = "Version"
     }
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        success = try container.decode(Bool.self, forKey: .success)
-        errorCode = try Int(container.decode(String.self, forKey: .errorCode))!
-        errorMessage = try? container.decode(String.self, forKey: .errorMessage)
-        errorDetails = try? container.decode(String.self, forKey: .errorDetails)
-        status = try container.decode(Status.self, forKey: .params)
-    }
+        let isAllowed = try container.decode(Bool.self, forKey: .isAllowed)
 
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(success, forKey: .success)
-        try container.encode(errorCode, forKey: .errorCode)
-        try? container.encode(errorMessage, forKey: .errorMessage)
-        try? container.encode(errorDetails, forKey: .errorDetails)
-        try container.encode(status, forKey: .params)
+        if isAllowed {
+            self = .allowed(try TinkoffPayMethod(from: decoder))
+        } else {
+            self = .disallowed
+        }
     }
 }
