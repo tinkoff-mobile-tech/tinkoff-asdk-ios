9c707e7a730584b217a5f0eca4e716b8c51a706e
Aleksandr Pravosudov <a.pravosudov@tinkoff.ru>
2023-03-21 13:59:11 +0400

    MIC-8251: Добавил TinkoffButton

---
+++ TinkoffASDKUI/TinkoffASDKUI/UIComponents/TinkoffButton/TinkoffButton.swift
@@ -0,0 +1,52 @@
+//
+//  TinkoffButton.swift
+//  TinkoffASDKUI
+//
+//  Created by Aleksandr Pravosudov on 21.03.2023.
+//
+
+public final class TinkoffButton: UIView {
+
+    // MARK: Properties
+
+    public var actionClosure: (() -> Void)?
+
+    private lazy var button: Button = {
+        let config = Button.Configuration(
+            title: "Оплатить с Тинькофф",
+            image: Asset.TinkoffPay.tinkoffPaySmallNoBorder.image,
+            style: .primaryTinkoff,
+            contentSize: .basicLarge,
+            imagePlacement: .trailing
+        )
+
+        let button = Button(configuration: config, action: { [weak self] in self?.actionClosure?() })
+        return button
+    }()
+
+    // MARK: Initialization
+
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Private
+
+extension TinkoffButton {
+    private func setupViews() {
+        backgroundColor = .clear
+        addSubview(button)
+    }
+
+    private func setupConstraints() {
+        button.makeEqualToSuperview()
+    }
+}
