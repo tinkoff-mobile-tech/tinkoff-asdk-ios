645d3d68c2760267808ca3dca55fe62485ddbf41
Aleksandr Pravosudov <a.pravosudov@tinkoff.ru>
2023-07-20 16:26:30 +0400

    EACQAPW-5604: Перегенерировал YandexPayPaymentFlowDelegateMock

--- TinkoffASDKUI/TinkoffASDKUITests/TestCases/YandexPayPaymentFlowTests.swift
+++ TinkoffASDKUI/TinkoffASDKUITests/TestCases/YandexPayPaymentFlowTests.swift
@@ -68,7 +68,7 @@ final class YandexPayPaymentFlowTests: BaseTestCase {
         sut.yandexPayPaymentSheet(completedWith: .cancelled())
 
         // then
-        XCTAssertEqual(yandexPayPaymentFlowDelegateMock.didCompleteWithCallsCount, 1)
-        XCTAssertEqual(yandexPayPaymentFlowDelegateMock.didCompleteWithReceivedArguments?.result, .cancelled())
+        XCTAssertEqual(yandexPayPaymentFlowDelegateMock.yandexPayPaymentFlowCallsCount, 1)
+        XCTAssertEqual(yandexPayPaymentFlowDelegateMock.yandexPayPaymentFlowReceivedArguments?.result, .cancelled())
     }
 }
