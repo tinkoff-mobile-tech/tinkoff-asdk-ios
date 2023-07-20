bf27115b54c3ee27fb2c487ac473d0f592016dc4
Nikita Vasilev <nv3212@gmail.com>
2023-07-20 13:42:13 +0400

    EQACQAPW-5597. Implement `ThreeDSURLBuilderTests`

---
+++ TinkoffASDKCore/TinkoffASDKCoreTests/Infrastructure/Mocks/URLProviderMock.swift
@@ -0,0 +1,18 @@
+//
+//  URLProviderMock.swift
+//  TinkoffASDKCore-Unit-Tests
+//
+//  Created by Никита Васильев on 20.07.2023.
+//
+
+import Foundation
+@testable import TinkoffASDKCore
+
+final class URLProviderMock: IURLProvider {
+    var url: URL {
+        get { return underlyingUrl }
+        set(value) { underlyingUrl = value }
+    }
+
+    var underlyingUrl: URL!
+}
