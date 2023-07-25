adcf52d5aa52fdb254584ff5482dcf520f6de85c
Nikita Vasilev <nv3212@gmail.com>
2023-07-25 17:24:46 +0400

    [EACQAPW-5669]: Implement `URLProviderTests` (#488)
    * Implement `URLProviderTests`
    
    * Update `CHANGELOG.md`

---
+++ TinkoffASDKCore/TinkoffASDKCoreTests/TestCases/URLProviderTests.swift
@@ -0,0 +1,40 @@
+//
+//  URLProviderTests.swift
+//  TinkoffASDKCore-Unit-Tests
+//
+//  Created by Никита Васильев on 25.07.2023.
+//
+
+@testable import TinkoffASDKCore
+import XCTest
+
+final class URLProviderTests: XCTestCase {
+    func test_buildURL() {
+        // given
+        let provider = URLProvider(host: .host)
+
+        // when
+        let url = provider?.url
+
+        // then
+        XCTAssertEqual(url?.absoluteString, "https://\(String.host)/")
+    }
+
+    func test_buildURL_whenHostIsInvalid() {
+        // given
+        let provider = URLProvider(host: .invalidHost)
+
+        // when
+        let url = provider?.url
+
+        // then
+        XCTAssertNil(url)
+    }
+}
+
+// MARK: Constants
+
+private extension String {
+    static let host = "host"
+    static let invalidHost = "хост"
+}
