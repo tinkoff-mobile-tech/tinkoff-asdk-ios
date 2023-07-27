4b9d87397fed506d38d34651a62a7731eb7afc50
Aleksandr Pravosudov <a.pravosudov@tinkoff.ru>
2023-07-27 12:33:07 +0400

    EACQAPW-5604: Перегенерировал AddNewCardAssemblyMock

--- TinkoffASDKUI/TinkoffASDKUITests/Infrastructure/Mocks/AddNewCardAssemblyMock.swift
+++ TinkoffASDKUI/TinkoffASDKUITests/Infrastructure/Mocks/AddNewCardAssemblyMock.swift
@@ -16,7 +16,7 @@ final class AddNewCardAssemblyMock: IAddNewCardAssembly {
 
     var addNewCardViewCallsCount = 0
     var addNewCardViewReceivedArguments: AddNewCardViewArguments?
-    var addNewCardViewReceivedInvocations: [AddNewCardViewArguments] = []
+    var addNewCardViewReceivedInvocations: [AddNewCardViewArguments?] = []
     var addNewCardViewReturnValue: AddNewCardViewController!
 
     func addNewCardView(customerKey: String, output: IAddNewCardPresenterOutput?, cardScannerDelegate: ICardScannerDelegate?) -> AddNewCardViewController {
@@ -32,17 +32,12 @@ final class AddNewCardAssemblyMock: IAddNewCardAssembly {
     typealias AddNewCardNavigationControllerArguments = (customerKey: String, cardScannerDelegate: ICardScannerDelegate?, onViewWasClosed: ((AddCardResult) -> Void)?)
 
     var addNewCardNavigationControllerCallsCount = 0
+    var addNewCardNavigationControllerReceivedArguments: AddNewCardNavigationControllerArguments?
+    var addNewCardNavigationControllerReceivedInvocations: [AddNewCardNavigationControllerArguments?] = []
     var addNewCardNavigationControllerOnViewWasClosedClosureInput: AddCardResult?
     var addNewCardNavigationControllerReturnValue: UINavigationController!
-    var addNewCardNavigationControllerReceivedArguments: AddNewCardNavigationControllerArguments?
-    var addNewCardNavigationControllerReceivedInvocations: [AddNewCardNavigationControllerArguments] = []
 
-    func addNewCardNavigationController(
-        customerKey: String,
-        cardScannerDelegate: ICardScannerDelegate?,
-        onViewWasClosed: ((AddCardResult) -> Void)?
-    ) -> UINavigationController {
-
+    func addNewCardNavigationController(customerKey: String, cardScannerDelegate: ICardScannerDelegate?, onViewWasClosed: ((AddCardResult) -> Void)?) -> UINavigationController {
         addNewCardNavigationControllerCallsCount += 1
         let arguments = (customerKey, cardScannerDelegate, onViewWasClosed)
         addNewCardNavigationControllerReceivedArguments = arguments
@@ -51,5 +46,20 @@ final class AddNewCardAssemblyMock: IAddNewCardAssembly {
             onViewWasClosed?(addNewCardNavigationControllerOnViewWasClosedClosureInput)
         }
         return addNewCardNavigationControllerReturnValue
+    }
+}
+
+// MARK: - Resets
+
+extension AddNewCardAssemblyMock {
+    func fullReset() {
+        addNewCardViewCallsCount = 0
+        addNewCardViewReceivedArguments = nil
+        addNewCardViewReceivedInvocations = []
+
+        addNewCardNavigationControllerCallsCount = 0
+        addNewCardNavigationControllerReceivedArguments = nil
+        addNewCardNavigationControllerReceivedInvocations = []
+        addNewCardNavigationControllerOnViewWasClosedClosureInput = nil
     }
 }
