9d0956a0622edd4314222e867d7e47b5c7bef51b
Ivan Glushko <ivanglushkodev@yandex.ru>
2023-08-08 21:38:27 +0400

    add card test 2

---
+++ TinkoffASDKUI/TinkoffASDKUI/Utils/AsdkAccessibilityIdentifier.swift
@@ -0,0 +1,12 @@
+//
+//  AsdkAccessibilityIdentifier.swift
+//  TinkoffASDKUI
+//
+//  Created by Ivan Glushko on 08.08.2023.
+//
+
+import Foundation
+
+public struct AsdkAccessibilityIdentifier {
+    public static let bankIconView = "bankIconView"
+}
